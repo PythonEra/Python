e0e2b9cde1f499bdaefaf472a9cb1bbc3c4b230a
kailas gorade <54238131+PythonEra@users.noreply.github.com>
2020-06-09 13:36:09 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,21 @@
+#Airthmetic
+a = int(input("Ente the first number "))
+b = int(input("Enter the secod number "))
+#sum
+sum = a + b
+#substracation
+
+sub = a - b
+# multiplication
+mul = a * b
+# division
+
+div = a/b
+# mod of givin number
+mod = a % b
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
